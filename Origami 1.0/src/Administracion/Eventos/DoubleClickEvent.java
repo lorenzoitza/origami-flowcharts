9d990798ba483a7ae1efc1e49d3ec969315f6d65
rodrigofdz <rodrigofdz@6316dd94-a7e6-11de-bf1c-ef771b8a44ac>
2009-10-26 15:12:08 +0000

    se cambio la clase EventoDobleClick a DoubleClickEvent y se renombraron variables ademas se idento el codigo

---
+++ Origami 1.0/src/Administracion/Eventos/DoubleClickEvent.java
@@ -0,0 +1,84 @@
+package Administracion.Eventos;
+
+import org.eclipse.draw2d.*;
+import Administracion.AdminSeleccion;
+import Administracion.Figura;
+import Grafico.*;
+import Grafico.Figuras.InputFigure;
+import Grafico.Figuras.ForFigure;
+import Grafico.Figuras.DecisionFigure;
+import Grafico.Figuras.OutputFigure;
+import Grafico.Figuras.SentenceFigure;
+import Grafico.Figuras.WhileFigure;
+import Grafico.VentanaDatos.InputFigureDialog;
+import Grafico.VentanaDatos.ForFigureDialog;
+import Grafico.VentanaDatos.OutputFigureDialog;
+import Grafico.VentanaDatos.DecisionFigureDialog;
+import Grafico.VentanaDatos.SentenceFigureDialog;
+import Grafico.VentanaDatos.WhileFigureDialog;
+import Administracion.TabFolder;
+/**
+ * Esta clase da la propiedad de doble click
+ * a las figuras del diagrama de flujo. 
+ * @version Origami 1.0
+ * @author Juan Ku, Victor Rodriguez
+ */
+public class DoubleClickEvent extends MouseListener.Stub{
+	public AdminSeleccion selectionAdmin;
+	public TabFolder tabFolder;
+	/**
+	 * Da la propiedad de Doble click 
+	 * a la figura recibida.
+	 * @param figure
+	 */
+	public  DoubleClickEvent(Figura figure,AdminSeleccion selectionAdmin,TabFolder tabFolder) {
+		figure.addMouseListener(this);  
+		this.selectionAdmin = selectionAdmin;
+		this.tabFolder = tabFolder;
+	}
+	/**
+	 * Este metodo reconoce en que figura
+	 * el evento se disparo y crea la ventana de datos.
+	 * @param MouseEvent
+	 */
+	    public void mouseDoubleClicked(MouseEvent event) {
+		Figura figure = ((Figura) event.getSource());
+		
+		if(figure instanceof SentenceFigure){
+			SentenceFigure setenceFigure = (SentenceFigure)figure;
+			
+			new SentenceFigureDialog(Ventana.shell,tabFolder
+				,setenceFigure,selectionAdmin).open();
+		}
+		else if(figure instanceof DecisionFigure){
+			DecisionFigure decisionFigure = ((DecisionFigure)figure);
+			new DecisionFigureDialog(Ventana.shell,tabFolder,
+				decisionFigure,selectionAdmin).open();
+		}
+		else if(figure instanceof InputFigure){
+			InputFigure inputFigure = ((InputFigure)figure);
+			
+			new InputFigureDialog(Ventana.shell,tabFolder
+				,inputFigure,selectionAdmin).open();
+		}
+		else if(figure instanceof OutputFigure){
+			OutputFigure outputFigure = ((OutputFigure)figure);
+			
+			new OutputFigureDialog(Ventana.shell,tabFolder
+				,outputFigure,selectionAdmin).open();
+		}
+		else if(figure instanceof ForFigure){
+			ForFigure forFigure = ((ForFigure)figure);
+			
+			new ForFigureDialog(Ventana.shell,tabFolder
+				,forFigure,selectionAdmin).open();
+		}
+		else if(figure instanceof WhileFigure){
+			WhileFigure whileFigure = ((WhileFigure)figure);
+			
+			new WhileFigureDialog(Ventana.shell,tabFolder
+				,whileFigure,selectionAdmin).open();
+			
+		}
+	}
+}
