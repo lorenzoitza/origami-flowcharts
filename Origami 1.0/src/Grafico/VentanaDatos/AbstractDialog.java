5fc234c2e68296fa0cb06fab58a205917df1d113
hudycr <hudycr@6316dd94-a7e6-11de-bf1c-ef771b8a44ac>
2009-10-29 06:54:54 +0000

    Extract Method para los botones en las clases abstractas de los dialogos

--- Origami 1.0/src/Grafico/VentanaDatos/AbstractDialog.java
+++ Origami 1.0/src/Grafico/VentanaDatos/AbstractDialog.java
@@ -34,21 +34,21 @@ public abstract class AbstractDialog<Figure> {
     }
     
     public AbstractDialog(Shell shell, TabFolder tabFolder, Figure figura,
-	    AdminSeleccion selectionAdmin) {
-	this.dialog = new Shell(shell, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
-
-	this.tabbedPaneSelected = tabFolder;
-
-	this.display = shell.getDisplay();
-
-	this.abstractFigure = figura;
-
-	this.selectionAdmin = selectionAdmin;
-
-	this.key = new EventoKey(selectionAdmin, tabFolder);
-
-	this.create();
-	this.initComponents();
+    		AdminSeleccion selectionAdmin) {
+		this.dialog = new Shell(shell, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
+	
+		this.tabbedPaneSelected = tabFolder;
+	
+		this.display = shell.getDisplay();
+	
+		this.abstractFigure = figura;
+	
+		this.selectionAdmin = selectionAdmin;
+	
+		this.key = new EventoKey(selectionAdmin, tabFolder);
+	
+		this.create();
+		this.initComponents();
     }
 
     public abstract void initLabels();
@@ -56,56 +56,63 @@ public abstract class AbstractDialog<Figure> {
     public abstract void initTextFields();
 
     protected void initButtons() {
-	acceptButton = new Button(dialog, SWT.FLAT);
-	acceptButton.setBounds(25, 85, 75, 25);
-	acceptButton.setText("ACEPTAR");
-	addSelectionListener(acceptButton, true);
-
-	cancelButton = new Button(dialog, SWT.FLAT);
-	cancelButton.setBounds(135, 85, 75, 25);
-	cancelButton.setText("CANCELAR");
-	addSelectionListener(cancelButton, false);
+    	initAcceptButton();
+    	initCancelButton();	
+    }
+    
+    private void initAcceptButton(){
+    	acceptButton = new Button(dialog, SWT.FLAT);
+		acceptButton.setBounds(25, 85, 75, 25);
+		acceptButton.setText("ACEPTAR");
+		addSelectionListener(acceptButton, true);
+    }
+    
+    private void initCancelButton(){
+    	cancelButton = new Button(dialog, SWT.FLAT);
+		cancelButton.setBounds(135, 85, 75, 25);
+		cancelButton.setText("CANCELAR");
+		addSelectionListener(cancelButton, false);
     }
 
     protected abstract void create();
 
     public void open(){
-	dialog.open();
-	while (!dialog.isDisposed()) {
-	    
-	    if (!display.readAndDispatch()) {
-		
-		display.sleep();
-	    }
-	}
+		dialog.open();
+		while (!dialog.isDisposed()) {
+		    
+		    if (!display.readAndDispatch()) {
+			
+			display.sleep();
+		    }
+		}
     }
 
     protected void initComponents() {
-	initLabels();
-	initTextFields();
-	initButtons();
+		initLabels();
+		initTextFields();
+		initButtons();
     }
     
     protected abstract void validate(boolean band);
 
     protected void addKeyListener(Text textField) {
-	textField.addKeyListener(getKeyListener());
+    	textField.addKeyListener(getKeyListener());
     }
     
     private KeyAdapter getKeyListener() {
-	return new org.eclipse.swt.events.KeyAdapter() {
-	    public void keyPressed(org.eclipse.swt.events.KeyEvent e) {
-		key.setKey(e);
-		if (key.PresentEnter()) {
-		    validate(true);
-		    dialog.close();
-		}
-	    }
-	};
+		return new org.eclipse.swt.events.KeyAdapter() {
+		    public void keyPressed(org.eclipse.swt.events.KeyEvent e) {
+			key.setKey(e);
+			if (key.PresentEnter()) {
+			    validate(true);
+			    dialog.close();
+			}
+		    }
+		};
     }
     
     protected void addSelectionListener(Button confirmButton, boolean band) {
-	confirmButton.addSelectionListener(getSelectionAdapter(band));
+    	confirmButton.addSelectionListener(getSelectionAdapter(band));
     }
     
     private SelectionAdapter getSelectionAdapter(final boolean band) {
--- Origami 1.0/src/Grafico/VentanaDatos/AbstractInputOutputDialog.java
+++ Origami 1.0/src/Grafico/VentanaDatos/AbstractInputOutputDialog.java
@@ -19,7 +19,7 @@ import Administracion.Eventos.EventoKey;
 import Imagenes.ImageLoader;
 
 public abstract class AbstractInputOutputDialog<Figure> extends
-	AbstractDialog<Figure> {
+		AbstractDialog<Figure> {
 
     protected ScrolledComposite scrolledComposite;
 
@@ -38,135 +38,146 @@ public abstract class AbstractInputOutputDialog<Figure> extends
     protected Label writeVariableLabel;
 
     public AbstractInputOutputDialog(Shell shell, TabFolder tabFolder,
-	    Figure figura, AdminSeleccion selectionAdmin) {
-	super();
-	this.dialog = new Shell(shell, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
-
-	this.tabbedPaneSelected = tabFolder;
-
-	this.display = shell.getDisplay();
-
-	this.abstractFigure = figura;
-
-	this.selectionAdmin = selectionAdmin;
-
-	this.key = new EventoKey(selectionAdmin, tabFolder);
-
-	this.scrolledComposite =
-		new ScrolledComposite(dialog, SWT.BORDER | SWT.H_SCROLL
-			| SWT.V_SCROLL);
-
-	this.composite = new Composite(scrolledComposite, SWT.NONE);
-
-	this.create();
-	this.initComponents();
+    		Figure figura, AdminSeleccion selectionAdmin) {
+		super();
+		this.dialog = new Shell(shell, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
+	
+		this.tabbedPaneSelected = tabFolder;
+	
+		this.display = shell.getDisplay();
+	
+		this.abstractFigure = figura;
+	
+		this.selectionAdmin = selectionAdmin;
+	
+		this.key = new EventoKey(selectionAdmin, tabFolder);
+	
+		this.scrolledComposite =
+			new ScrolledComposite(dialog, SWT.BORDER | SWT.H_SCROLL
+				| SWT.V_SCROLL);
+	
+		this.composite = new Composite(scrolledComposite, SWT.NONE);
+	
+		this.create();
+		this.initComponents();
     }
 
     protected void initComponents() {
-	initScrollComposite();
-
-	initLabels();
-
-	initButtons();
-
-	initTextFields();
+		initScrollComposite();
+	
+		initLabels();
+	
+		initButtons();
+	
+		initTextFields();
     }
 
     protected void initScrollComposite() {
-	GridData grid = new GridData(GridData.FILL_VERTICAL);
-	grid.grabExcessHorizontalSpace = true;
-	grid.grabExcessVerticalSpace = true;
-	scrolledComposite.setLayoutData(grid);
-	composite.setLayoutData(grid);
-
-	scrolledComposite.setContent(composite);
-	scrolledComposite.setExpandHorizontal(true);
-	scrolledComposite.setExpandVertical(true);
-	scrolledComposite.setBounds(0, 20, 325, 120);
-
-	scrolledComposite.setMinSize(composite.computeSize(SWT.DEFAULT, SWT.DEFAULT,
-		false));
+		GridData grid = new GridData(GridData.FILL_VERTICAL);
+		grid.grabExcessHorizontalSpace = true;
+		grid.grabExcessVerticalSpace = true;
+		scrolledComposite.setLayoutData(grid);
+		composite.setLayoutData(grid);
+	
+		scrolledComposite.setContent(composite);
+		scrolledComposite.setExpandHorizontal(true);
+		scrolledComposite.setExpandVertical(true);
+		scrolledComposite.setBounds(0, 20, 325, 120);
+	
+		scrolledComposite.setMinSize(composite.computeSize(SWT.DEFAULT, SWT.DEFAULT,
+			false));
     }
 
     @Override
     protected void initButtons() {
-	acceptButton = new Button(dialog, SWT.FLAT);
-	acceptButton.setBounds(5, 145, 70, 25);
-	acceptButton.setText("ACEPTAR");
-	addSelectionListener(acceptButton, true);
-
-	cancelButton = new Button(dialog, SWT.FLAT);
-	cancelButton.setBounds(85, 145, 70, 25);
-	cancelButton.setText("CANCELAR");
-	addSelectionListener(cancelButton, false);
-
-	addTextFieldButton = new Button(dialog, SWT.PUSH);
-	addTextFieldButton.setSize(45, 40);
-	addTextFieldButton.setLocation(318, 140);
-	addTextFieldButton.setImage(ImageLoader.getImage("suma.png"));
-	addTextFieldButton.pack();
-	addTextSelectionListener();
+    	initAcceptButton();
+    	initCancelButton();
+    	initTextFieldButton();
+    }
+    
+    private void initAcceptButton(){
+    	acceptButton = new Button(dialog, SWT.FLAT);
+		acceptButton.setBounds(5, 145, 70, 25);
+		acceptButton.setText("ACEPTAR");
+		addSelectionListener(acceptButton, true);
+    }
+    
+    private void initCancelButton(){
+    	cancelButton = new Button(dialog, SWT.FLAT);
+		cancelButton.setBounds(85, 145, 70, 25);
+		cancelButton.setText("CANCELAR");
+		addSelectionListener(cancelButton, false);
+    }
+    
+    private void initTextFieldButton(){
+    	addTextFieldButton = new Button(dialog, SWT.PUSH);
+		addTextFieldButton.setSize(45, 40);
+		addTextFieldButton.setLocation(318, 140);
+		addTextFieldButton.setImage(ImageLoader.getImage("suma.png"));
+		addTextFieldButton.pack();
+		addTextSelectionListener();
     }
 
     protected void addTextListener(Text textField) {
-	textField.addListener(SWT.FocusIn, getFocusListener(textField));
+    	textField.addListener(SWT.FocusIn, getFocusListener(textField));
     }
 
     private Listener getFocusListener(final Text text) {
-	return new Listener() {
-
-	    public void handleEvent(Event e) {
-		if (text.getText().startsWith("Escribe")) {
-		    text.setText("");
-		}
-	    }
-	};
+		return new Listener() {
+	
+		    public void handleEvent(Event e) {
+			if (text.getText().startsWith("Escribe")) {
+			    text.setText("");
+			}
+		    }
+		};
     }
 
     protected void addDeleteButton(int location) {
-	final Button deleteButton = new Button(composite, SWT.PUSH);
-	deleteButton.setBounds(280, location * 25, 20, 20);
-	deleteButton.setImage(ImageLoader.getImage("borrar.gif"));
-	deleteButton.addSelectionListener(new SelectionAdapter() {
-
-	    public void widgetSelected(SelectionEvent event) {
-		for (int x = 0; x < scrolledCompositeContent.length; x += numHorizComponents) {
-		    if (scrolledCompositeContent[x].getBounds().y == deleteButton.getBounds().y) {
-			Text text = (Text) scrolledCompositeContent[x];
-			text.setText("");
+		final Button deleteButton = new Button(composite, SWT.PUSH);
+		
+		deleteButton.setBounds(280, location * 25, 20, 20);
+		deleteButton.setImage(ImageLoader.getImage("borrar.gif"));
+		deleteButton.addSelectionListener(new SelectionAdapter() {
+	
+		    public void widgetSelected(SelectionEvent event) {
+			for (int x = 0; x < scrolledCompositeContent.length; x += numHorizComponents) {
+			    if (scrolledCompositeContent[x].getBounds().y == deleteButton.getBounds().y) {
+				Text text = (Text) scrolledCompositeContent[x];
+				text.setText("");
+			    }
+			}
 		    }
-		}
-	    }
-	});
+		});
     }
 
     protected abstract void addTextComponent(int position);
 
     protected void addTextField(int position) {
-	int verticalSpace = 25;
-	Text text = new Text(composite, SWT.FLAT | SWT.BORDER);
-	text.setBounds(0, position * verticalSpace, 250, 20);
-	text.setText("Escribe aqui");
-
-	addTextListener(text);
-
-	addKeyListener(text);
+		int verticalSpace = 25;
+		Text text = new Text(composite, SWT.FLAT | SWT.BORDER);
+		text.setBounds(0, position * verticalSpace, 250, 20);
+		text.setText("Escribe aqui");
+	
+		addTextListener(text);
+	
+		addKeyListener(text);
     }
 
     protected void addTextSelectionListener() {
-	addTextFieldButton.addSelectionListener(getTextSelectionAdapter());
+    	addTextFieldButton.addSelectionListener(getTextSelectionAdapter());
     }
 
     private SelectionAdapter getTextSelectionAdapter() {
-	return new SelectionAdapter() {
-
-	    public void widgetSelected(SelectionEvent e) {
-		addTextComponent(composite.getChildren().length
-			/ numHorizComponents);
-		scrolledCompositeContent = composite.getChildren();
-		scrolledComposite.setMinSize(composite.computeSize(SWT.DEFAULT,
-			SWT.DEFAULT, false));
-	    }
-	};
+		return new SelectionAdapter() {
+	
+		    public void widgetSelected(SelectionEvent e) {
+			addTextComponent(composite.getChildren().length
+				/ numHorizComponents);
+			scrolledCompositeContent = composite.getChildren();
+			scrolledComposite.setMinSize(composite.computeSize(SWT.DEFAULT,
+				SWT.DEFAULT, false));
+		    }
+		};
     }
 }
