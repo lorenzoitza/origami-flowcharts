ae47c5183d8e5d76c58aa1631cc76218e547279b
JuanKuQ <JuanKuQ@6316dd94-a7e6-11de-bf1c-ef771b8a44ac>
2009-12-14 03:32:32 +0000

    Propagacion de la eliminacion de la logica en Grafico

--- Origami 1.0/src/ui/events/AddFigureEvent.java
+++ Origami 1.0/src/ui/events/AddFigureEvent.java
@@ -4,53 +4,37 @@ import org.eclipse.draw2d.Figure;
 import org.eclipse.draw2d.MouseEvent;
 import org.eclipse.draw2d.MouseListener;
 import org.eclipse.draw2d.MouseMotionListener;
-import org.eclipse.draw2d.geometry.Point;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Cursor;
 
 import Administracion.AdminSeleccion;
 import Administracion.TabFolder;
 import Administracion.Eventos.EventoAgregarFigura;
 
-
 public class AddFigureEvent extends MouseMotionListener.Stub implements MouseListener{
-	private Point start;
-	public boolean bandera=false;
-	public final Cursor[] cursor = new Cursor[1];
-	public int cursorPrincipal = SWT.CURSOR_ARROW;
-	public AdminSeleccion selec;
-	public TabFolder tab;
-	public EventoAgregarFigura ev;
-	/**
-	 * Da la propiedad de Drag & Drop 
-	 * a la figura recibida.
-	 * @param figure
-	 */
-	public AddFigureEvent(Figure figure,AdminSeleccion selecc,TabFolder tabfolder) {
-		figure.addMouseMotionListener(this);
-		figure.addMouseListener(this);
-		selec = selecc;
-		tab = tabfolder;
-		ev = new EventoAgregarFigura(selecc,tabfolder);
-	}
-	/**
-	 * Recorre el diagrama de figuras y verifica si
-	 * la figura fue liberada en un area disponible.
-	 * @param MouseEvent 
-	 */
-	public void mouseReleased(MouseEvent e){
-	}
-	public void mouseClicked(MouseEvent e) {
-  	}
-	public void mouseDoubleClicked(MouseEvent e) {
-  	}
-	/**
-	 * Obtiene la localizacion en la que la figura fue seleccionada.
-	 * @param MouseEvent 
-	 */
-	public void mousePressed(MouseEvent e) {
-	    ev.mousePresseds(e);
-	}
-	public void mouseDragged(MouseEvent e) {
-	}
+    private EventoAgregarFigura ev;
+	
+    /**
+     * Da la propiedad de Drag & Drop a la figura recibida.
+     * @param figure
+     */
+    public AddFigureEvent(Figure figure, AdminSeleccion selecc, TabFolder tabfolder) {
+	figure.addMouseMotionListener(this);
+	figure.addMouseListener(this);
+	ev = new EventoAgregarFigura(selecc,tabfolder);
+    }
+
+    public void mouseReleased(MouseEvent e){
+    }
+	
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    public void mouseDoubleClicked(MouseEvent e) {
+    }
+	
+    public void mousePressed(MouseEvent e) {
+	ev.mousePresseds(e);
+    }
+	
+    public void mouseDragged(MouseEvent e) {
+    }
 }
