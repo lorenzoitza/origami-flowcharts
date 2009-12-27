de6e0ce02b35ee106d02d0be4078c82e51bd86b4
JuanKuQ <JuanKuQ@6316dd94-a7e6-11de-bf1c-ef771b8a44ac>
2009-12-27 00:27:07 +0000

    Union eventos con actions

---
+++ Origami 1.0/src/ui/actions/ChangeCursorEvent.java
@@ -0,0 +1,56 @@
+package ui.actions;
+
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.MouseEvent;
+import org.eclipse.draw2d.MouseListener;
+import org.eclipse.draw2d.MouseMotionListener;
+
+import Administracion.TabFolder;
+import Administracion.actions.EventoCambiarCursor;
+import Grafico.CustomMenu;
+
+
+public class ChangeCursorEvent  extends MouseMotionListener.Stub implements MouseListener{
+	public boolean bandera=false;
+	private EventoCambiarCursor ev;
+	public TabFolder tab;
+	/**
+	 * Da la propiedad de Drag & Drop 
+	 * a la figura recibida.
+	 * @param figure
+	 */
+	public ChangeCursorEvent(IFigure figure,TabFolder tabfolder){
+        	figure.addMouseMotionListener(this);
+        	figure.addMouseListener(this);
+        	tab = tabfolder;
+        	ev = new EventoCambiarCursor(figure,tabfolder);
+	}
+	/**
+	 * Recorre el diagrama de figuras y verifica si
+	 * la figura fue liberada en un area disponible.
+	 * @param MouseEvent 
+	 */
+	public void mouseReleased(MouseEvent e){
+	    CustomMenu._editMenu.menuDisponibleFigura();
+	}
+	public void mouseClicked(MouseEvent e) {
+  	}
+	public void mouseDoubleClicked(MouseEvent e) {
+  	}
+	public void mouseMoved(MouseEvent me) {
+		ev.mouseMoveds(me);
+	}
+	/**
+	 * Obtiene la localizacion en la que la figura fue seleccionada.
+	 * @param MouseEvent 
+	 */
+	public void mousePressed(MouseEvent e) {
+	}
+	/**
+	 * Obtiene la diferencia en la que la figura fue seleccionada 
+	 * y la distancia a donde fue movida para actualizar su posicion.
+	 * @param MouseEvent 
+	 */
+	public void mouseDragged(MouseEvent e) {
+	}
+}
