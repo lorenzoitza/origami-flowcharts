bb09827998cdd7b7454a721435bde1c4b95bdaa7
victor.fmat <victor.fmat@6316dd94-a7e6-11de-bf1c-ef771b8a44ac>
2009-12-27 20:56:03 +0000

    Agregando clases de Listerner para code C y code cpp

---
+++ Origami 1.0/src/ui/listener/ViewCodeCppAction.java
@@ -0,0 +1,28 @@
+package ui.listener;
+
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+
+import Administracion.Funcionalidad.Codigo.Manager;
+import Grafico.MainWindow;
+import Grafico.view.DiagramCodeView;
+
+
+public class ViewCodeCppAction implements SelectionListener{
+
+    @Override
+    public void widgetDefaultSelected(SelectionEvent arg0) {
+	// TODO Auto-generated method stub
+	
+    }
+
+    @Override
+    public void widgetSelected(SelectionEvent arg0) {
+	  Manager manager = new Manager(MainWindow.getComponents()._diagrams.getHoja().getDiagrama());
+	  manager.formatCodeCpp();
+	  DiagramCodeView view = new DiagramCodeView(manager.getInstructionsFormat());
+	  view.createWindow();
+	  view.show();
+    }
+
+}
